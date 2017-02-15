e9b177fb5fa77d82c68b57186707aa8693a94545
lupyana <lupiaovic21@gmail.com>
2017-02-15 14:06:19 +0300

    wednesday update

--- public/js/init.js
+++ public/js/init.js
@@ -2,6 +2,7 @@
   $(function(){
 
     $('.button-collapse').sideNav();
+     $('#tabs-swipe-demo').tabs({ 'swipeable': true });
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
