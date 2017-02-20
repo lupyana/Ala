b7e5d4b33fc29be5cb129afa385f487d3f404c4b
lupyana <lupianovic21@gmail.com>
2017-02-20 18:43:31 +0300

    Custom Css and Js files

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,7 +21,6 @@
     <link rel="manifest" href="{{ asset('manifest.json') }}">
     <link rel="mask-icon" href="{{ asset('safari-pinned-tab.svg') }}" color="#5bbad5">
     <meta name="theme-color" content="#ffffff">
->
 
     <!-- Scripts -->
     <script>
