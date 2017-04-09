3039de70df57d8c115a7f538b01ca204c56b3de2
lupyana <lupianovic21@gmail.com>
2017-04-09 11:22:07 +0300

    Modify seeds and restrict user to add lesson

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -111,7 +111,7 @@
             <ul>
               <a href="{{ route('lessonRequest') }}"><li>Request a Lesson or Tabs</li></a>
             <!-- <a href="#">  <li>Submit a Tab</li></a> -->
-              <!-- <a href="{{ route('submitLesson') }}"><li>Submit a Lesson</li></a> -->
+            remo  <!-- <a href="{{ route('submitLesson') }}"><li>Submit a Lesson</li></a> -->
             </ul>
           </div>
         </div>
